ae9ea63484d050212ddf27670844b4e1d6e1c42f
Jun-soundsleep <nnamjun94@gmail.com>
2023-03-01 15:06:48 +0900

    FIX: get_filtered_boards_with_pagination function in READ board api

--- app/mapper/repository/board/repository_board.py
+++ app/mapper/repository/board/repository_board.py
@@ -33,7 +33,7 @@ class BoardRepository(IBoardRepository):
         return self.db.query(Boards).filter(Boards.title.ilike(f"%{q}%")).all()
 
     def get_filtered_boards_with_pagination(self, q: str, skip: int = 0, limit: int = 10) -> List[Boards]:
-        return self.db.query(Boards).filterBoards.title.ilike(f"%{q}%").offset(skip).limit(limit).all()
+        return self.db.query(Boards).filter(Boards.title.ilike(f"%{q}%")).offset(skip).limit(limit).all()
 
     def delete_board_item(self, id: int) -> bool:
         self.db.query(Boards).filter(Boards.id == id).delete()
