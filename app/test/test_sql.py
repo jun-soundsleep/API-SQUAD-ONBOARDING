f416859ab2b637487332c0f9fabbc9666e0402c8
Jun-soundsleep <nnamjun94@gmail.com>
2023-03-03 13:44:47 +0900

    TEST: board patch, delete api

--- app/test/test_sql.py
+++ app/test/test_sql.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker, Session
 from fastapi.testclient import TestClient
 
 from app.main import app
@@ -26,3 +26,9 @@ def override_get_db():
 app.dependency_overrides[get_db] = override_get_db
 
 client = TestClient(app)
+
+
+def delete_table(table_model):
+    with Session(bind=engine) as session:
+        session.query(table_model).delete()
+        session.commit()
